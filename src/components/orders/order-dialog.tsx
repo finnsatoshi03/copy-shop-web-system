0cb3329c5b40c75006cfeb5cd416ba2e9514e632
finnsatoshi03 <finnsatoshi03@gmail.com>
2024-09-06 20:34:09 +0800

    refactor: Update OrderDialog UI to make it even

--- src/components/orders/order-dialog.tsx
+++ src/components/orders/order-dialog.tsx
@@ -173,71 +173,75 @@ export function OrderDialog({
         </DialogHeader>
 
         <form onSubmit={handleSubmit(onSubmit)} className="h-[80vh]">
-          <div className="-mx-[1.6rem] -mt-8 h-full">
-            <div className="relative h-1/2">
-              {orderDetails?.image ? (
-                <img
-                  src={orderDetails.image}
-                  className="h-full w-full object-cover"
-                  alt={orderDetails.name}
-                  style={{
-                    objectPosition: "50% 25%",
-                  }}
-                />
-              ) : (
-                <div className="flex h-full w-full items-center justify-center bg-gray-100">
-                  <span className="text-gray-500">No image available</span>
+          <div className="flex h-full flex-col justify-between pb-4">
+            <div className="-mx-[1.6rem] -mt-8 h-full">
+              <div className="relative h-1/2">
+                {orderDetails?.image ? (
+                  <img
+                    src={orderDetails.image}
+                    className="h-full w-full object-cover"
+                    alt={orderDetails.name}
+                    style={{
+                      objectPosition: "50% 25%",
+                    }}
+                  />
+                ) : (
+                  <div className="flex h-full w-full items-center justify-center bg-gray-100">
+                    <span className="text-gray-500">No image available</span>
+                  </div>
+                )}
+                <div className="absolute -bottom-4 right-8 inline-flex items-center gap-4 rounded-full bg-yellow-500 px-2 py-2 font-label text-sm">
+                  <button type="button" onClick={decrementQuantity}>
+                    <Minus size={16} />
+                  </button>
+                  <p className="font-bold">{quantity}</p>
+                  <button type="button" onClick={incrementQuantity}>
+                    <Plus size={16} />
+                  </button>
                 </div>
-              )}
-              <div className="absolute -bottom-4 right-8 inline-flex items-center gap-4 rounded-full bg-yellow-500 px-2 py-2 font-label text-sm">
-                <button type="button" onClick={decrementQuantity}>
-                  <Minus size={16} />
-                </button>
-                <p className="font-bold">{quantity}</p>
-                <button type="button" onClick={incrementQuantity}>
-                  <Plus size={16} />
-                </button>
+              </div>
+
+              <div className="mt-8 px-8">
+                <div className="flex justify-between">
+                  <h1 className="w-2/3 font-label text-xl font-bold">
+                    {orderDetails.name || "Unnamed Beverage"}
+                  </h1>
+                  <div className="flex items-center gap-1 text-xs">
+                    <Flame size={12} className="text-orange-600" />
+                    <p className="leading-none">{calories || "N/A"} Calories</p>
+                  </div>
+                </div>
+                <p className="font-label text-xs opacity-60">
+                  {orderDetails.description || "No description available"}
+                </p>
+
+                <h2 className="mt-4 font-label font-bold">Sizes for you</h2>
+                <div className="mt-1 flex items-center gap-2">
+                  {sizeButtons}
+                </div>
+
+                <h2 className="mt-4 font-label font-bold">Sugar Level</h2>
+                <div className="mt-1 flex flex-col">
+                  <Slider
+                    defaultValue={[0]}
+                    max={100}
+                    min={0}
+                    step={25}
+                    value={[sugar_level]}
+                    onValueChange={handleSugarLevelChange}
+                  />
+                  <div className="mt-2 flex justify-between text-xs font-bold">
+                    <span>0%</span>
+                    <span>25%</span>
+                    <span>50%</span>
+                    <span>75%</span>
+                    <span>100%</span>
+                  </div>
+                </div>
               </div>
             </div>
 
-            <div className="mt-8 px-8">
-              <div className="flex justify-between">
-                <h1 className="w-2/3 font-label text-xl font-bold">
-                  {orderDetails.name || "Unnamed Beverage"}
-                </h1>
-                <div className="flex items-center gap-1 text-xs">
-                  <Flame size={12} className="text-orange-600" />
-                  <p className="leading-none">{calories || "N/A"} Calories</p>
-                </div>
-              </div>
-              <p className="font-label text-xs opacity-60">
-                {orderDetails.description || "No description available"}
-              </p>
-
-              <h2 className="mt-4 font-label font-bold">Sizes for you</h2>
-              <div className="mt-1 flex items-center gap-2">{sizeButtons}</div>
-
-              <h2 className="mt-4 font-label font-bold">Sugar Level</h2>
-              <div className="mt-1 flex flex-col">
-                <Slider
-                  defaultValue={[0]}
-                  max={100}
-                  min={0}
-                  step={25}
-                  value={[sugar_level]}
-                  onValueChange={handleSugarLevelChange}
-                />
-                <div className="mt-2 flex justify-between text-xs font-bold">
-                  <span>0%</span>
-                  <span>25%</span>
-                  <span>50%</span>
-                  <span>75%</span>
-                  <span>100%</span>
-                </div>
-              </div>
-            </div>
-
-            <div className="mb-4 mt-6 flex w-full justify-between px-8">
+            <div className="flex w-full justify-between">
               <div className="flex flex-col justify-between">
                 <h2 className="text-xs font-medium opacity-60">Total Price:</h2>
                 <p className="flex items-end gap-1 text-2xl font-bold">
