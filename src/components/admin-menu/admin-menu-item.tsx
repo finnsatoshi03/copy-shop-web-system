54e9ad7225247066d69a442f5825112cd93ff475
finnsatoshi03 <finnsatoshi03@gmail.com>
2024-09-11 03:43:13 +0800

    refactor: Update package dependencies and add popover component
    This commit updates the package dependencies in the package-lock.json file, adding the "@radix-ui/react-popover" package. It also adds a new popover component in the src/components/ui/popover.tsx file, which allows for displaying content in a popover style. These changes improve the functionality and user experience of the admin menu in the application.

--- src/components/admin-menu/admin-menu-item.tsx
+++ src/components/admin-menu/admin-menu-item.tsx
@@ -1,21 +1,74 @@
 import { useState } from "react";
 import { Beverage } from "@/lib/types";
 import CreateNewItemDialog from "@/components/admin-menu/create-new-dialog";
+import { EllipsisVertical, Trash2, XCircle } from "lucide-react";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
 
 export default function AdminMenuItem({ data }: { data: Beverage }) {
   const [isOpen, setIsOpen] = useState(false);
 
+  const handleDelete = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    console.log(`Deleting item: ${data.name}`);
+  };
+
+  const handleMarkUnavailable = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    console.log(`Marking as unavailable: ${data.name}`);
+  };
+
   return (
-    <div className="flex cursor-pointer gap-2" onClick={() => setIsOpen(true)}>
-      <img src={data.image} className="size-16 rounded-md" />
-      <div>
-        <h1 className="line-clamp-1 max-w-xl font-bold leading-none">
-          {data.name}
-        </h1>
-        <p className="line-clamp-2 max-w-xl text-sm opacity-60">
-          {data.description}
-        </p>
+    <div
+      className="flex cursor-pointer items-center justify-between pr-2"
+      onClick={() => setIsOpen(true)}
+    >
+      <div className="flex gap-2">
+        <img src={data.image} className="size-16 rounded-md" />
+        <div>
+          <h1 className="line-clamp-1 max-w-xl font-bold leading-none">
+            {data.name}
+          </h1>
+          <p className="line-clamp-2 max-w-xl text-sm opacity-60">
+            {data.description}
+          </p>
+        </div>
       </div>
+
+      <Popover>
+        <PopoverTrigger asChild>
+          <EllipsisVertical
+            size={16}
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+          />
+        </PopoverTrigger>
+        <PopoverContent className="w-fit" side="left">
+          <div className="flex flex-col gap-2">
+            <Button
+              variant="link"
+              className="flex h-fit items-center justify-start gap-2 p-0 text-xs text-red-600"
+              onClick={handleDelete}
+            >
+              <Trash2 size={14} />
+              Delete
+            </Button>
+            <Button
+              variant="link"
+              className="flex h-fit items-center justify-start gap-2 p-0 text-xs"
+              onClick={handleMarkUnavailable}
+            >
+              <XCircle size={14} />
+              Mark Unavailable
+            </Button>
+          </div>
+        </PopoverContent>
+      </Popover>
 
       <CreateNewItemDialog
         beverageData={data}
