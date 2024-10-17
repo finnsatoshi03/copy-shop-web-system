8373fc43b4097bc52848a650b1bc19a0e871ab2c
Ashley Lumibao <ashp.lumibao@gmail.com>
2024-10-17 22:46:19 +0800

    Added script for updating files that uses localhost in data fetching and also the host that runs the server

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  server: {
+    host: '192.168.0.103',
+    port: 3000
+  },
   plugins: [react()],
   resolve: {
     alias: {
