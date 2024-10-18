f56ee6b272096c5ad6e0d23055187f16ead4a8ba
finnsatoshi03 <finnsatoshi03@gmail.com>
2024-10-19 01:34:37 +0800

    Refactor landing page text to update cafe name

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -57,7 +57,7 @@ export default function LandingPage() {
           <p className="text-center text-xs md:text-sm">
             Scan our QR code and step into a new era of coffee ordering. Enjoy
             personalized menus, quick service, and delicious brews tailored to
-            your taste. Amiga Cafe: Your coffee, your way, your QR.
+            your taste. RKYVE: Your coffee, your way, your QR.
           </p>
         </motion.div>
         <motion.div
